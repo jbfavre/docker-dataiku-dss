97140c065303266241ce1f12e3fd16b5171814b5
Jean Baptiste Favre <debian@jbfavre.org>
2016-06-15 15:00:27 +0200

    Initial commit for DSS 3.0.3 on Debian Jessie

---
+++ docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+DATAIKUDATA=/var/lib/dataiku
+DATAIKUPROG=/home/dataiku
+
+# Dataiku Science Studio should be shut down properly
+function shut_down() {
+  echo "Shutting Down Dataiku Science Studio"
+  /bin/su - dataiku -c "${DATAIKUDATA}/bin/dss stop"
+  exit
+}
+
+trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+
+/bin/chown dataiku:dataiku ${DATAIKUDATA}
+
+if [ -f ${DATAIKUDATA}/bin/dss ]; then
+  echo "Upgrading Dataiku Science Studio"
+  /bin/su - dataiku -c "cd ${DATAIKUPROG} && ./installer.sh -d ${DATAIKUDATA} -u -y"
+else
+  echo "Installing Dataiku Science Studio"
+  /bin/su - dataiku -c "cd ${DATAIKUPROG} && ./installer.sh -d ${DATAIKUDATA} -p 10000"
+fi
+
+if [ -f ${DATAIKUDATA}/bin/dss ]; then
+  /bin/su - dataiku -c "${DATAIKUDATA}/bin/dss start"
+else
+  exit 1
+fi
+
+echo "Dataiku Science Studio is now running"
+
+while true; do
+  sleep 1
+done
