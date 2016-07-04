30aa886d7e5de77f1578166f8d3c88bb60c59b94
Jean Baptiste Favre <debian@jbfavre.org>
2016-07-04 16:41:23 +0200

    Update docker-entrypoint.sh scripts for design, automation & api nodes

--- snode/docker-entrypoint.sh
+++ snode/docker-entrypoint.sh
@@ -3,26 +3,54 @@ set -e
 
 DATAIKUDATA=/var/lib/dataiku
 DATAIKUPROG=/home/dataiku
+DATAIKULICENSE=/etc/dataiku/license.json
 
-# Dataiku Science Studio should be shut down properly
-function shut_down() {
-  echo "Shutting Down Dataiku Automation node"
-  /bin/su - dataiku -c "${DATAIKUDATA}/bin/dss stop"
-  exit
-}
+IS_DSS_CONFIGURED=0
+HAS_DSS_LICENSE=0
 
-trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+# Override ownership of DSS data directory
+# Avoid issue if modifications have been made on host side
+/bin/chown -R dataiku:dataiku ${DATAIKUDATA}
 
-/bin/chown dataiku:dataiku ${DATAIKUDATA}
-
+# Check Dataiku installation status
+IS_DSS_CONFIGURED=0
 if [ -f ${DATAIKUDATA}/bin/dss ]; then
-  echo "Upgrading Dataiku Automation node"
-  /bin/su - dataiku -c "cd ${DATAIKUPROG} && ./installer.sh -t api -d ${DATAIKUDATA} -u -y"
-else
-  echo "Installing Dataiku Automation node"
-  /bin/su - dataiku -c "cd ${DATAIKUPROG} && ./installer.sh -t api -d ${DATAIKUDATA} -p 12000"
+    IS_DSS_CONFIGURED=1
+fi
+# Check license status
+# API node requires a valid license to start
+HAS_DSS_LICENSE=0
+if [ -f ${DATAIKULICENSE} ]; then
+    HAS_DSS_LICENSE=1
 fi
 
+# Define installer options to be run
+# Depends on DSS being already installed or not
+# Depends on custom license being provided or not
+if [ ${IS_DSS_CONFIGURED} -eq 0 -a ${HAS_DSS_LICENSE} -eq 0 ];
+then
+    echo "* DSS is not installed"
+    echo "* No custom license file found"
+    echo "** Installing Dataiku as API Node with community license"
+    DSSINSTALLOPT=""
+elif [ ${IS_DSS_CONFIGURED} -eq 0 -a ${HAS_DSS_LICENSE} -eq 1 ];
+then
+    echo "* DSS is not installed"
+    echo "* Custom license file found"
+    echo "** Installing Dataiku as API Node with custom license"
+    DSSINSTALLOPT="-l ${DATAIKULICENSE} -p 12000"
+elif [ ${IS_DSS_CONFIGURED} -eq 1 ];
+then
+    echo "* DSS hass already been installed"
+    echo "** Upgrading Dataiku API Node"
+    DSSINSTALLOPT="-u -y"
+else
+    echo "*** Oooops… Shouldn't be there :-/"
+    exit 1
+fi
+/bin/su - dataiku -c "cd ${DATAIKUPROG} && ./installer.sh -t api -d ${DATAIKUDATA} ${DSSINSTALLOPT}"
+
+# Once installed or upgraded, we can start DSS
 if [ -f ${DATAIKUDATA}/bin/dss ]; then
   /bin/su - dataiku -c "${DATAIKUDATA}/bin/dss run"
 else
